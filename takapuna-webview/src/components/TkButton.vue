2b3919a6f2a094b0b0aee43cd49b7bca3749065c
Phillip Rak <rak.phillip@gmail.com>
2022-07-04 10:19:13 -0700

    Reduce repetition in button class
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- takapuna-webview/src/components/TkButton.vue
+++ takapuna-webview/src/components/TkButton.vue
@@ -15,30 +15,24 @@ const buttonClass = computed(() => {
     'outline-1': true, 
     'outline-transparent': true, 
     'outline-offset-2': true, 
-    'bg-tk-button-background': true, 
-    'text-tk-button-foreground': true,
     'hover:cursor-pointer': true,
-    'hover:bg-tk-button-hoverBackground': true,
     'focus:outline-tk-focusBorder': true
   }
   if (props.primary) {
-    return button;
+    return {
+      ...button,
+      'bg-tk-button-background': true, 
+      'text-tk-button-foreground': true,
+      'hover:bg-tk-button-hoverBackground': true,
+    };
   }
 
   if (props.secondary) {
     return {
-      'w-full': true,
-      'p-2': true,
-      'text-center': true,
-      'outline': true, 
-      'outline-1': true, 
-      'outline-transparent': true, 
-      'outline-offset-2': true, 
+      ...button,
       'bg-tk-button-secondaryBackground': true, 
       'text-tk-button-secondaryForeground': true,
-      'hover:cursor-pointer': true,
-      'hover:bg-tk-button-secondaryHoverBackground': true,
-      'focus:outline-tk-focusBorder': true
+      'hover:bg-tk-button-secondaryHoverBackground': true
     }
   }
 
